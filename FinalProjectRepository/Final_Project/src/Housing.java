bfd4e305bb4e87a0c0f2b2fe490e556b9a6229c8
Megan Hansen <meganhansen62@gmail.com>
2016-04-22 14:57:13 -0600

    Finished(?) Housing class

--- FinalProjectRepository/Final_Project/src/Housing.java
+++ FinalProjectRepository/Final_Project/src/Housing.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Housing {
 
 private double housingDuration = 0.0;
+private double housingPayment = 0.0;
 private char paymentPeriod = 'W';
 public double housingCost = 0.0;
 
@@ -16,7 +17,7 @@ public double housingCost = 0.0;
 	public char getPaymentPeriod(){
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("How often will you be making housing payments?"
-				+ "/n" + "Enter W for weekly, M for monthly, and S if one bulk payment will be paid: ");
+				+ "\n" + "Enter W for weekly, M for monthly, and S if one bulk payment will be paid: ");
 		String paymentPeriodString = scnr.next();
 		scnr.close();
 		paymentPeriod = paymentPeriodString.charAt(0);
@@ -25,16 +26,24 @@ public double housingCost = 0.0;
 	public double getHousingPayment(){
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Enter your weekly/monthly/total housing payment,"
-				+ "/n" + "based on the letter you entered above: ");
-		String paymentPeriodString = scnr.next();
+				+ "\n" + "based on the letter you entered above: ");
+		housingPayment = scnr.nextDouble();
 		scnr.close();
-		paymentPeriod = paymentPeriodString.charAt(0);
-		return paymentPeriod;	
+		return housingPayment;
 	}
 	public double getHousingCost(){
-		/*if (paymentPeriod == 'W'){
-			housingCost = 
-		}*/
+		if (paymentPeriod == 'W'){
+			housingCost = housingPayment * housingDuration;
+		}else if(paymentPeriod == 'M'){
+			housingCost = housingPayment * (housingDuration/4.0);
+		}else if(paymentPeriod == 'S'){
+			housingCost = housingPayment;
+		}else{
+			System.out.println("Error. You must enter W, M, or H to the question:"
+					+ "\n" + "How often will you be making housing payments?");
+			getPaymentPeriod();
+			getHousingCost();
+		}
 		return housingCost;
 	}
 }
