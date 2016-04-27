ff953fb2c6aec4044eacb429b8ea14202418c462
johnalley71 <johnalley71@192.168.0.245>
2016-04-27 01:23:27 -0600

    More GUI stuff

--- FinalProjectRepository/Final_Project/src/Main.java
+++ FinalProjectRepository/Final_Project/src/Main.java
@@ -1,7 +1,21 @@
 import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		
+		try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception unused) {
+            ; // Ignore exception because we can't do anything.  Will use default.
+        }
+        
+		GUI window = new GUI();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);	
 	
 	Income Income = new Income(); //Creates New Income Class
 	
